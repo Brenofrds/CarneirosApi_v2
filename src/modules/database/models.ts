d52dba69929626d0935a8a04277da3bcc7f63f05
viniciusmagao <vincius.engredes@gmail.com>
2025-02-07 22:46:24 -0300

    Atualiza models.ts: agente, hospede

--- src/modules/database/models.ts
+++ src/modules/database/models.ts
@@ -1,8 +1,48 @@
 import prisma from '../../config/database';
 
 /**
- * Busca todos os proprietários que ainda não foram sincronizados com o Jestor.
+ * Busca todos os agentes que ainda não foram sincronizados com o Jestor.
  */
+export async function getAgentesNaoSincronizados() {
+    const agentes = await prisma.agente.findMany({
+        where: {
+            sincronizadoNoJestor: false, // Filtra apenas os agentes não sincronizados
+        },
+    });
+    
+    // Verifica se existe registro para sincronizar
+    if(agentes.length === 0){
+        console.log("Todos os agentes ja estao sincronizados!");
+        return false;
+    } 
+    else {
+        return agentes;
+    }    
+}
+
+/**
+ * Busca todos os agentes que ainda não foram sincronizados com o Jestor.
+ */
+export async function getHospedesNaoSincronizados() {
+    const hospede = await prisma.hospede.findMany({
+        where: {
+            sincronizadoNoJestor: false, // Filtra apenas os agentes não sincronizados
+        },
+    });
+    
+    // Verifica se existe registro para sincronizar
+    if(hospede.length === 0){
+        console.log("Todos os hospedes ja estao sincronizados!");
+        return false;
+    } 
+    else {
+        return hospede;
+    }    
+}
+
+/**
+ * Busca todos os proprietários que ainda não foram sincronizados com o Jestor.
+ /
 export async function getProprietariosNaoSincronizados() {
     const proprietarios = await prisma.proprietario.findMany({
         where: {
@@ -12,17 +52,7 @@ export async function getProprietariosNaoSincronizados() {
   
     return proprietarios;
 }
+*/
 
-/**
- * Busca todos os agentes que ainda não foram sincronizados com o Jestor.
- */
-export async function getAgentesNaoSincronizados() {
-    const agentes = await prisma.agente.findMany({
-        where: {
-            sincronizadoNoJestor: false, // Filtra apenas os agentes não sincronizados
-        },
-    });
-  
-    return agentes;
-}
+
   
