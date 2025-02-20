c3ca787086daa1f78d5b7b9513915fd18aed208c
viniciusmagao <vincius.engredes@gmail.com>
2025-02-20 02:42:24 -0300

    Revisao do arquivo jestor.service.ts

--- src/modules/jestor/jestor.service.ts
+++ src/modules/jestor/jestor.service.ts
@@ -1,51 +1,50 @@
-import {    
-    sincronizarAgentes,
-    sincronizarBloqueios,
-    sincronizarCanais,
-    sincronizarCondominios,
-    sincronizarHospedes,
-    sincronizarImoveis,
-    sincronizarProprietarios,
-    sincronizarReservas,
-    sincronizarTaxasReservas
-} from './services/agentes.service';
+import { sincronizarAgente } from './services/agentes.service';
+import { sincronizarBloqueio } from './services/bloqueios.service';
+import { sincronizarCanal } from './services/canais.service';
+import { sincronizarCondominio } from './services/condominios.service';
+import { sincronizarHospede } from './services/hospedes.service';
+import { sincronizarImovel } from './services/imoveis.service';
+import { sincronizarProprietario } from './services/proprietarios.service';
+import { sincronizarReserva } from './services/reservas.service';
+import { sincronizarTaxaReserva } from './services/taxasReservas.service';
+
 
 export async function sincronizarTudo() {
     try {
         console.log('Iniciando sincronização de agentes...');
-        await sincronizarAgentes();
+        await sincronizarAgente();
         console.log('Sincronização de agentes concluída!');
 
         console.log('Iniciando sincronização de bloqueios...');
-        await sincronizarBloqueios();
+        await sincronizarBloqueio();
         console.log('Sincronização de bloqueios concluída!');
 
         console.log('Iniciando sincronização de canais...');
-        await sincronizarCanais();
+        await sincronizarCanal();
         console.log('Sincronização de canais concluída!');
 
         console.log('Iniciando sincronização de condomínios...');
-        await sincronizarCondominios();
+        await sincronizarCondominio();
         console.log('Sincronização de condomínios concluída!');
 
         console.log('Iniciando sincronização de hóspedes...');
-        await sincronizarHospedes();
+        await sincronizarHospede();
         console.log('Sincronização de hóspedes concluída!');
 
         console.log('Iniciando sincronização de imóveis...');
-        await sincronizarImoveis();
+        await sincronizarImovel();
         console.log('Sincronização de imóveis concluída!');
 
         console.log('Iniciando sincronização de proprietários...');
-        await sincronizarProprietarios();
+        await sincronizarProprietario();
         console.log('Sincronização de proprietários concluída!');
 
         console.log('Iniciando sincronização de reservas...');
-        await sincronizarReservas();
+        await sincronizarReserva();
         console.log('Sincronização de reservas concluída!');
 
         console.log('Iniciando sincronização de taxas de reservas...');
-        await sincronizarTaxasReservas();
+        await sincronizarTaxaReserva();
         console.log('Sincronização de taxas de reservas concluída!');
         
     } catch (error: any) {
