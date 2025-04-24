195b7f7214fa56f13746980ba70922f5b770960f
Breno Soares Fernandes <breno.fernandes@serpro.gov.br>
2025-04-24 16:33:07 -0300

    Ajustes app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "";
-import routes from "./routes/routes";
+import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 
